d74e0e8f1d48a22c58660d5f44505291d8a669a8
tungit04315 <93088015+tungit04315@users.noreply.github.com>
2023-09-08 23:19:09 +0700

    commit file pom

---
+++ RealEstate_FAB5/src/main/java/com/poly/bean/Pay.java
@@ -0,0 +1,18 @@
+package com.poly.bean;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Pay {
+	
+	int pay_id;
+	
+}
