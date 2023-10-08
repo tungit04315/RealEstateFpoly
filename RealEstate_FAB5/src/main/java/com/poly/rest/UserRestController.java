46fd7c5664e464a7b7c828ebb0e7ba6e0566cf28
tungit04315 <93088015+tungit04315@users.noreply.github.com>
2023-10-08 11:26:23 +0700

    test rest api

--- RealEstate_FAB5/src/main/java/com/poly/rest/UserRestController.java
+++ RealEstate_FAB5/src/main/java/com/poly/rest/UserRestController.java
@@ -1,6 +1,7 @@
 package com.poly.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,9 @@ import com.poly.util.SessionService;
 public class UserRestController {
 
 	@Autowired
+	BCryptPasswordEncoder pe;
+	
+	@Autowired
 	UsersService uService;
 	
 	@Autowired
@@ -23,5 +27,31 @@ public class UserRestController {
 	public Users getUser() {
 		Users u = (Users) ss.getAttribute("user");
 		return uService.findById(u.getUsername());
+	}
+	
+	@RequestMapping("/login-test")
+	public String Users() {
+		
+		Users u = uService.findById("user1");
+		
+		u.setPasswords(pe.encode(u.getPasswords()));
+		
+		uService.create(u);
+		//userS1@
+		return u.getPasswords();
+	}
+	
+	@RequestMapping("/login-test-2")
+	public String UsersMatches() {
+		
+		Users u = uService.findById("user1");
+		
+		if(pe.matches("userS1@", u.getPasswords())) {
+			return u.getPasswords();
+		}else {
+			return "Failed";
+		}
+		//userS1@
+		
 	}
 }
