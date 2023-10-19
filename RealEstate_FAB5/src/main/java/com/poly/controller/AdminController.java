541b66b578c43496bc3390cf96f9c53db210f0cf
MSI\PHAT <danhthuthinhphat@gmail.com>
2023-10-20 00:39:52 +0700

    Admin ChangePass

--- RealEstate_FAB5/src/main/java/com/poly/controller/AdminController.java
+++ RealEstate_FAB5/src/main/java/com/poly/controller/AdminController.java
@@ -1,26 +1,35 @@
 package com.poly.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.poly.bean.Users;
 import com.poly.service.UsersService;
+import com.poly.util.ParamService;
 import com.poly.util.SessionService;
 
 @Controller
 public class AdminController {
 
 	@Autowired
+	ParamService paramService;
+	
+	@Autowired
 	UsersService userService;
 	
 	@Autowired
 	SessionService ss;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	// Home
 	@RequestMapping({"/admin","/admin/index"})
 	public String getHome(Model m) {
+		
 		return "admin/index";
 	}
 	// Home
@@ -28,6 +37,7 @@ public class AdminController {
 	// User List
 	@RequestMapping({"/admin/users","/admin/user-list"})
 	public String getUsers(Model m) {
+		
 		return "admin/users";
 	}
 	// User List
@@ -56,7 +66,8 @@ public class AdminController {
 	// Profile User (Admin)
 	@RequestMapping("/admin/profile")
 	public String getProfile(Model m) {
-		m.addAttribute("u", userService.findById("tungngayngo")); 
+		m.addAttribute("u", userService.findById("Phatbieu"));
+		ss.setAttribute("user",userService.findById("Phatbieu"));
 		return "admin/profile";
 	}
 	
@@ -64,7 +75,28 @@ public class AdminController {
 	public String setProfile(Model m, Users u) {
 		userService.update(u);
 		ss.setAttribute("user", u);
-		return "redirect:/admin/profile";
+		return "redirect:/admin/profile";	
+	}
+	@RequestMapping("/admin/ChangePass")
+	public String setChangePass(Model m, Users u) {
+		Users user = (Users) ss.getAttribute("user");
+
+		String passmoi = paramService.getString("passmoi", "");
+		String nhaplaipassmoi = paramService.getString("nhaplaipassmoi", "");
+
+		System.out.println(u.getPasswords());
+		System.out.println(user.getPasswords());
+
+			if (passmoi.equalsIgnoreCase(nhaplaipassmoi)) {
+				u.setPasswords(passwordEncoder.encode(passmoi));// db
+				user.setPasswords(passwordEncoder.encode(passmoi));// session
+				userService.update(user);
+				m.addAttribute("successPass", true);
+				return "redirect:/admin/profile";
+			} else {
+				m.addAttribute("errorPass", true);
+				return "redirect:/admin/profile";
+			}
 	}
 	// Profile User (Admin)
 }
