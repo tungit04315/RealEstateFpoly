2aadd7adac483dee58bad9383f423cd768228874
tungit04315 <93088015+tungit04315@users.noreply.github.com>
2023-11-14 09:20:39 +0700

    commit dev

--- RealEstate_FAB5/src/main/java/com/poly/rest/UserRestController.java
+++ RealEstate_FAB5/src/main/java/com/poly/rest/UserRestController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -62,5 +64,15 @@ public class UserRestController {
 		}
 		//userS1@
 		
+	}
+	
+	@GetMapping("/user-find")
+	public Users getFind(@Param("id") String id) {
+		return uService.findById(id);
+	}
+	
+	@PutMapping("/update-user")
+	public Users getUpdateUser(@RequestBody Users u) {
+		return uService.update(u);
 	}
 }
