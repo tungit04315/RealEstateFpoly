34e17938308fe70d2ce78bdf21446ec37caf2345
tungit04315 <93088015+tungit04315@users.noreply.github.com>
2023-12-07 11:38:41 +0700

    commit test

--- RealEstate_FAB5/src/main/java/com/poly/dao/DetailTransactionsDAO.java
+++ RealEstate_FAB5/src/main/java/com/poly/dao/DetailTransactionsDAO.java
@@ -9,7 +9,7 @@ import com.poly.bean.DetailTransactions;
 
 public interface DetailTransactionsDAO extends JpaRepository<DetailTransactions, Integer>{
 
-	@Query(value="select * from detail_transactions d where d.transactions_id in (select t.transactions_id from transactions t where t.users = ?1)", nativeQuery = true)
+	@Query(value="select * from detail_transactions d where d.transactions_id in (select t.transactions_id from transactions t where t.users = ?1) order by d.detail_id desc", nativeQuery = true)
 	public List<DetailTransactions> getDetailTransactionsByUser(String username);
 	
 	@Query(value="select top 10 * from detail_transactions where transactions_type = 'true' order by transactions_id desc", nativeQuery = true)
